39d72bc9de5f9499ae9243bb4b1b20dc247ce417
Rafael de Andrade Prenholato <52474736+Rafa3lP@users.noreply.github.com>
2022-03-25 11:29:14 -0300

    retirar linhas adicionadas pelo git

--- alegre-mais-limpo/src/pages/Index.vue
+++ alegre-mais-limpo/src/pages/Index.vue
@@ -1,17 +1,7 @@
 <template>
-<<<<<<< Updated upstream
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
-  </q-page>
-=======
     <div class="q-pa-md">
         <center><q-img src="~assets/logo.png"></q-img></center>
     </div> 
->>>>>>> Stashed changes
 </template>
 
 <script>
